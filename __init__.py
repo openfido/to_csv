881cd195a199ab96533e552eda5dbe84abda2179
David P. Chassin <dchassin@slac.stanford.edu>
2020-11-04 14:21:58 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,8 +10,15 @@ OUTPUTS
 
   List of CSV output files. If output omitted, output is written to /dev/stdout.
 
+OPTIONS
+
+	--json-<option>=<value>    JSON reader options (see pandas.read_json documentation)
+	--csv-<option>=<value>     CSV writer options (see pandas.DataFrame to_csv documentation)
+
 """
 import os, csv, pandas, json
+from openfido_util import *
+
 def main(inputs,outputs,options):
 	if not inputs:
 		inputs = ["/dev/stdin"]
@@ -26,16 +33,30 @@ def main(inputs,outputs,options):
 			format = "dict"
 		elif option in ["-l","--list"]:
 			format = "list"
+		elif option[0:7] == "--json-":
+			specs = option.split("=")
+			if len(specs) < 2:
+				raise Exception(f"'{option}' is not valid") 
+			name = specs[0][7:]
+			value = specs[1]	
+			json_options[name] = get_option(name,value,json_option_types[name])
+		elif option[0:6] == "--csv-":
+			specs = option.split("=")
+			if len(specs) < 2:
+				raise Exception(f"'{option}' is not valid") 
+			name = specs[0][6:]
+			value = specs[1]	
+			csv_options[name] = get_option(name,value,csv_option_types[name])
 		else:
-			raise Exception("'{option}' is not a valid JSON format")
+			raise Exception(f"'{option}' is not a valid JSON format")
 	result = []
 	for file in inputs:
 		if not file:
 			raise Exception("missing input")
 		if file != "/dev/stdin" and file[-5:] != ".json":
-			raise Exception(f"{file} is not in a supported input format")	
-		data = pandas.read_json(file)
+			raise Exception(f"{file} is not in a supported input format")
+		data = pandas.read_json(file,**json_options)
 		result.append(data)
 	data = pandas.concat(result)
-	data.to_csv(outputs[0],header=False,index=False)
+	data.to_csv(outputs[0],**csv_options)
 	return data
