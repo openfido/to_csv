15bbf3bc249f61402d3c17ee2dfce5eff330be75
David P. Chassin <dchassin@slac.stanford.edu>
2020-11-12 07:26:19 -0800

    Add openfido_util and refactor code to use it

--- __init__.py
+++ __init__.py
@@ -16,47 +16,14 @@ OPTIONS
 	--csv-<option>=<value>     CSV writer options (see pandas.DataFrame to_csv documentation)
 
 """
-import os, csv, pandas, json
-from openfido_util import *
 
 def main(inputs,outputs,options):
-	if not inputs:
-		inputs = ["/dev/stdin"]
-	if outputs:
-		if len(outputs) > 1 :
-			raise Exception("too many outputs")
-	else:
-		outputs = ["/dev/stdout"]
-	format = "dict"
-	for option in options:
-		if option in ["-d","--dict"]:
-			format = "dict"
-		elif option in ["-l","--list"]:
-			format = "list"
-		elif option[0:7] == "--json-":
-			specs = option.split("=")
-			if len(specs) < 2:
-				raise Exception(f"'{option}' is not valid") 
-			name = specs[0][7:]
-			value = specs[1]	
-			json_options[name] = get_option(name,value,json_option_types[name])
-		elif option[0:6] == "--csv-":
-			specs = option.split("=")
-			if len(specs) < 2:
-				raise Exception(f"'{option}' is not valid") 
-			name = specs[0][6:]
-			value = specs[1]	
-			csv_options[name] = get_option(name,value,csv_option_types[name])
-		else:
-			raise Exception(f"'{option}' is not a valid JSON format")
+	import pandas
+	import openfido_util as of
+	of.setup_io(inputs,outputs)
 	result = []
 	for file in inputs:
-		if not file:
-			raise Exception("missing input")
-		if file != "/dev/stdin" and file[-5:] != ".json":
-			raise Exception(f"{file} is not in a supported input format")
-		data = pandas.read_json(file,**json_options)
-		result.append(data)
+		result.append(of.read_input(file,options))
 	data = pandas.concat(result)
-	data.to_csv(outputs[0],**csv_options)
-	return data
+	of.write_output(data,outputs[0],options)
+	return {outputs[0]:data}
